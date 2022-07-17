e23fe4ad24115fcd17c5509a8d704b3320d5d00a
Guilhermo Hounie <hounie.guilhermo@gmail.com>
2022-07-16 22:56:05 -0300

    index page about

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,14 +12,14 @@ const IndexPage: NextPage = () => {
         <h2>Guilhermo Hounie</h2>
         <div className="grid gap-4">
           <p>
-            Hello, I'm Guilhermo Hounie. I'm a computer scientist, web developer and enthusiast
-            in many things. I try to explain the world around me with code.
+            Hello, I'm Guilhermo Hounie. I'm a computer science student, web developer and
+            enthusiast in many things. I try to explain the world around me with code.
           </p>
           <p>
             I love the web and its technologies, my goal is to contribute to the creation of
             amazing applications while helping to make the web a better place.
           </p>
-          <p>I'm a minimalist, lover of retro computing, future funk, jazz and cats.</p>
+          <p>I'm a minimalist, lover of retro computing, shoegaze and cats.</p>
           <p>
             I love the sun and nature, I love hiking and my favorite food is sushi. My dream is
             to live in peace and harmony with myself and all things around me.
