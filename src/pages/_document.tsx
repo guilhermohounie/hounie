92d1ef5a6e80377a5707d743d02a68381d992f9e
Guilhermo Hounie <hounie.guilhermo@gmail.com>
2022-07-20 14:29:16 -0300

    lora font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,13 @@ import { FC } from "react";
 const Document: FC<DocumentProps> = () => {
   return (
     <Html dir="ltr" lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
